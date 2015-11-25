35e0d03b338d7ced80605c8015f991a32b3b411d
stephen-standridge <stephen@formisfunction.io>
2015-11-24 16:19:48 -0800

    initial commit

---
+++ src/reducer.js
@@ -0,0 +1,9 @@
+import {setEntries, next, vote, INITIAL_STATE} from './core.js'
+export default function reducer(state = INITIAL_STATE, action){
+	switch(action.type){
+		case 'NEXT': return next(state)
+		case 'VOTE': return state.update('vote', voteState => vote(voteState, action.entry))
+		case 'SET_ENTRIES': return setEntries(state, action.entries)
+		default: return state
+	}
+}
